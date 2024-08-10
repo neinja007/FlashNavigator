a14d28a8e4061779805d44b12f7da3777bc3e06e
neinja007 <anton.siligan@gmail.com>
2024-08-10 11:13:10 +0200

    add propmt to install extension

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -17,6 +17,12 @@ export default function Home({ params }: { params: { slug?: string[] } }) {
 	const [shortcuts, setShortcuts] = useState<Shortcut[]>([]);
 	const [hide, setHide] = useState(false);
 
+	const [extensionPropmt, setExtensionPrompt] = useState(true);
+
+	useEffect(() => {
+		setExtensionPrompt(localStorage.getItem('extensionPrompt') === 'true');
+	}, []);
+
 	useEffect(() => {
 		setHide(localStorage.getItem('hide') === 'true');
 	}, [hide]);
@@ -135,6 +141,46 @@ export default function Home({ params }: { params: { slug?: string[] } }) {
 					</div>
 				</div>
 			</div>
+			{!extensionPropmt && (
+				<>
+					<div className='fixed inset-0 w-full h-full bg-black opacity-50' onClick={() => setData('')} />
+					<div className='fixed bg-gray-700 shadow-lg rounded-xl inset-0 mx-auto my-auto p-5 w-[700px] h-fit text-xl'>
+						<p>
+							Please install an extension <b>to redirect new tabs to this page</b>:{' '}
+						</p>
+						<br />
+						<div className='text-blue-400 underline ml-3 space-y-3'>
+							<Link href={'https://addons.mozilla.org/en-GB/firefox/addon/new-tab-override'} className='block mb-3'>
+								Extension For Firefox
+							</Link>
+							<Link
+								href={'https://chromewebstore.google.com/detail/new-tab-redirect/icpgjfneehieebagbmdbhnlpiopdcmna'}
+								className='block mb-3'
+							>
+								Extension For Google
+							</Link>
+						</div>
+						<br />
+						<p>
+							After installing, use this URL as the new tab URL: <br />
+							<b className='text-blue-400 underline'>flash-navigator.vercel.app</b>{' '}
+							<button onClick={() => navigator.clipboard.writeText('https://flash-navigator.vercel.app')}>
+								[copy link]
+							</button>
+						</p>
+						<br />
+						<button
+							className='px-2 bg-blue-500 active:bg-blue-600 rounded-lg w-full py-1'
+							onClick={() => {
+								localStorage.setItem('extensionPrompt', 'true');
+								setExtensionPrompt(true);
+							}}
+						>
+							{'Done'}
+						</button>
+					</div>
+				</>
+			)}
 			{data && (
 				<>
 					<div className='fixed inset-0 w-full h-full bg-black opacity-25' onClick={() => setData('')} />
