2dcb555e0f4976db40e11fd62a114be8a8fb9b77
neinja007 <anton.siligan@gmail.com>
2024-08-13 08:59:28 +0200

    remove hide

--- src/components/shortcut.tsx
+++ src/components/shortcut.tsx
@@ -8,7 +8,6 @@ type ShortcutProps = {
 	group: boolean | undefined;
 	href: string | undefined;
 	img: string | undefined;
-	hideIfEmpty?: boolean;
 	setGroups: Dispatch<SetStateAction<string[]>>;
 };
 
@@ -21,47 +20,45 @@ function isValidURL(string: string) {
 	}
 }
 
-const Shortcut = ({ name, group, href, img, setShortcutId, hideIfEmpty, setGroups }: ShortcutProps) => {
+const Shortcut = ({ name, group, href, img, setShortcutId, setGroups }: ShortcutProps) => {
 	return (
-		(!hideIfEmpty || name) && (
-			<div
-				className='mb-12 rounded-lg h-fit sm:aspect-square cursor-pointer hover:bg-gray-700 pb-2'
-				onContextMenu={(e) => {
-					e.preventDefault();
-					setShortcutId();
-				}}
-				onClick={
-					name
-						? group
-							? () => setGroups((prev) => [...prev, name.replace(' ', '_')])
-							: () => {
-									window.location.href = href ? (/^https?:\/\//i.test(href) ? href : 'http://' + href) : '';
-								}
-						: setShortcutId
-				}
-			>
-				<div className='relative mx-auto w-full h-16 sm:h-full overflow-hidden rounded-t-lg mb-3'>
-					{img && img.startsWith('https://external-content.duckduckgo.com') && isValidURL(img) ? (
-						<Image
-							alt='The Icon has Failed to Load'
-							className='m-auto max-w-16 max-h-16 h-16 sm:h-auto sm:max-w-32 sm:max-h-32'
-							src={img}
-							fill
-							sizes='100px'
-						/>
-					) : (
-						<span className='text-6xl sm:text-9xl select-none'>?</span>
-					)}
-				</div>
-				{name ? (
-					<b className='line-clamp-2'>
-						{group ? '📁' : '🔗'} {name}
-					</b>
+		<div
+			className='mb-12 rounded-lg h-fit sm:aspect-square cursor-pointer hover:bg-gray-700 pb-2'
+			onContextMenu={(e) => {
+				e.preventDefault();
+				setShortcutId();
+			}}
+			onClick={
+				name
+					? group
+						? () => setGroups((prev) => [...prev, name.replace(' ', '_')])
+						: () => {
+								window.location.href = href ? (/^https?:\/\//i.test(href) ? href : 'http://' + href) : '';
+							}
+					: setShortcutId
+			}
+		>
+			<div className='relative mx-auto w-full h-16 sm:h-full overflow-hidden rounded-t-lg mb-3'>
+				{img && img.startsWith('https://external-content.duckduckgo.com') && isValidURL(img) ? (
+					<Image
+						alt='The Icon has Failed to Load'
+						className='m-auto max-w-16 max-h-16 h-16 sm:h-auto sm:max-w-32 sm:max-h-32'
+						src={img}
+						fill
+						sizes='100px'
+					/>
 				) : (
-					<span className='text-blue-300 underline cursor-pointer'>Edit Shortcut</span>
+					<span className='text-6xl sm:text-9xl select-none'>?</span>
 				)}
 			</div>
-		)
+			{name ? (
+				<b className='line-clamp-2'>
+					{group ? '📁' : '🔗'} {name}
+				</b>
+			) : (
+				<span className='text-blue-300 underline cursor-pointer'>Edit Shortcut</span>
+			)}
+		</div>
 	);
 };
 
