41c7530b2e8243c61366a4747e6de319a79d502a
neinja007 <anton.siligan@gmail.com>
2024-08-13 08:36:06 +0200

    limit routes

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -88,15 +88,15 @@ export default function Home() {
 				<div className='mt-8'>
 					<div className='sm:flex justify-between pb-5'>
 						<div>
-							{['', ...groups].map((group, i) => (
+							{['', ...groups].slice(Math.max(groups.length - 2, 0)).map((group, i) => (
 								<Fragment key={i}>
 									<button
-										onClick={() => setGroups((prev) => prev.slice(0, i))}
-										className='px-2 py-1 mx-2 rounded-lg bg-blue-800 text-white'
+										onClick={() => setGroups((prev) => prev.slice(0, prev.length - (2 - i)))}
+										className='max-w-[150px] truncate px-2 py-1 mx-2 rounded-lg bg-blue-800 text-white'
 									>
 										{group || 'Home'}
 									</button>
-									{groups.length !== i && '>'}
+									{i !== Math.min(2, groups.length) && '>'}
 								</Fragment>
 							))}
 						</div>
