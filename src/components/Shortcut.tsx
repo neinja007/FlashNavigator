fdcc2bb3c5da7dfd4dcf8327882b5797132d51e1
neinja007 <anton.siligan@gmail.com>
2024-08-14 08:37:52 +0200

    refactor shortcut props and clean up eventhandlers

--- src/components/Shortcut.tsx
+++ src/components/Shortcut.tsx
@@ -1,3 +1,4 @@
+import { ShortcutType } from '@/app/page';
 import { addHTTPProtocolToUrl } from '@/utils/addHTTPProtocolToUrl';
 import { convertUrlToExternalImageUrl } from '@/utils/convertUrlToExternalImageUrl';
 import Image from 'next/image';
@@ -5,39 +6,45 @@ import { Dispatch, SetStateAction } from 'react';
 
 type ShortcutProps = {
 	setShortcutId: () => void;
-	name: string | undefined;
-	group: boolean | undefined;
-	href: string | undefined;
-	img: string | undefined;
+	shortcut: ShortcutType;
 	setGroups: Dispatch<SetStateAction<string[]>>;
+	queryResult?: boolean;
+	resetSearchBarQuery?: () => void;
 };
 
-const Shortcut = ({ name, group, href, img, setShortcutId, setGroups }: ShortcutProps) => {
+const Shortcut = ({ setShortcutId, setGroups, queryResult, shortcut, resetSearchBarQuery }: ShortcutProps) => {
 	return (
 		<div
-			className='mb-12 rounded-lg h-fit sm:aspect-square cursor-pointer hover:bg-gray-700 pb-2'
+			className='mb-12 rounded-lg h-fit sm:aspect-square cursor-pointer pb-2 hover:bg-gray-700'
 			onContextMenu={(e) => {
+				if (queryResult) return;
 				e.preventDefault();
 				setShortcutId();
 			}}
-			onClick={
-				name && (href || group)
-					? group
-						? () => setGroups((prev) => [...prev, name.replace(' ', '_')])
-						: href
-							? () => {
-									window.location.href = addHTTPProtocolToUrl(href);
-								}
-							: setShortcutId
-					: setShortcutId
-			}
+			onClick={() => {
+				if (shortcut.name) {
+					if (shortcut.group) {
+						if (queryResult) {
+							if (!resetSearchBarQuery) throw new Error('resetSearchBarQuery is not defined');
+							resetSearchBarQuery();
+						}
+						setGroups([...shortcut.path, shortcut.name]);
+					} else if (shortcut.href) {
+						window.location.href = addHTTPProtocolToUrl(shortcut.href);
+					} else {
+						setShortcutId();
+					}
+				} else {
+					setShortcutId();
+				}
+			}}
 		>
 			<div className='relative mx-auto w-full h-16 sm:h-full overflow-hidden rounded-t-lg mb-3'>
-				{img ? (
+				{shortcut.img ? (
 					<Image
 						alt='The Icon has Failed to Load'
 						className='m-auto max-w-16 max-h-16 h-16 sm:h-auto sm:max-w-32 sm:max-h-32'
-						src={convertUrlToExternalImageUrl(img)}
+						src={convertUrlToExternalImageUrl(shortcut.img)}
 						fill
 						sizes='100px'
 					/>
@@ -45,9 +52,9 @@ const Shortcut = ({ name, group, href, img, setShortcutId, setGroups }: Shortcut
 					<span className='text-6xl sm:text-9xl select-none'>?</span>
 				)}
 			</div>
-			{name ? (
+			{shortcut.name ? (
 				<b className='line-clamp-2'>
-					{group ? '📁' : '🔗'} {name}
+					{shortcut.group ? '📁' : '🔗'} {shortcut.name}
 				</b>
 			) : (
 				<span className='text-blue-300 underline cursor-pointer'>Edit Shortcut</span>
