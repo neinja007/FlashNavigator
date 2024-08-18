8aff8d197d6b9dd3087295f1b6bc6334ea7904ef
neinja007 <anton.siligan@gmail.com>
2024-08-18 06:10:20 +0200

    implement setting functionality

--- src/components/Shortcut.tsx
+++ src/components/Shortcut.tsx
@@ -1,4 +1,5 @@
 import { ShortcutType } from '@/app/page';
+import { useStorageState } from '@/hooks/useStorageState';
 import { addHTTPProtocolToUrl } from '@/utils/addHTTPProtocolToUrl';
 import { convertUrlToExternalImageUrl } from '@/utils/convertUrlToExternalImageUrl';
 import Image from 'next/image';
@@ -13,55 +14,62 @@ type ShortcutProps = {
 };
 
 const Shortcut = ({ setShortcutId, setGroups, queryResult, shortcut, resetSearchBarQuery }: ShortcutProps) => {
+	const [hideIcons] = useStorageState('settings-hide_shortcut_icons');
+	const [hideEmptyShortcuts] = useStorageState('settings-hide_empty_shortcuts');
+	const [imageQuality] = useStorageState('settings-image_quality');
+
 	return (
-		<div
-			className='mb-12 rounded-lg h-fit sm:aspect-square cursor-pointer pb-2 hover:bg-gray-700'
-			onContextMenu={(e) => {
-				if (queryResult) return;
-				e.preventDefault();
-				setShortcutId();
-			}}
-			onClick={() => {
-				if (shortcut.name) {
-					if (shortcut.group) {
-						if (queryResult) {
-							if (!resetSearchBarQuery) throw new Error('resetSearchBarQuery is not defined');
-							resetSearchBarQuery();
+		(hideEmptyShortcuts === 'false' || shortcut.name || shortcut.href || shortcut.img) && (
+			<div
+				className='mb-12 rounded-lg h-fit sm:aspect-square cursor-pointer pb-2 hover:bg-gray-700'
+				onContextMenu={(e) => {
+					if (queryResult) return;
+					e.preventDefault();
+					setShortcutId();
+				}}
+				onClick={() => {
+					if (shortcut.name) {
+						if (shortcut.group) {
+							if (queryResult) {
+								if (!resetSearchBarQuery) throw new Error('resetSearchBarQuery is not defined');
+								resetSearchBarQuery();
+							}
+							setGroups([...shortcut.path, shortcut.name]);
+						} else if (shortcut.href) {
+							window.location.href = addHTTPProtocolToUrl(shortcut.href);
+						} else {
+							!queryResult && setShortcutId();
 						}
-						setGroups([...shortcut.path, shortcut.name]);
-					} else if (shortcut.href) {
-						window.location.href = addHTTPProtocolToUrl(shortcut.href);
 					} else {
 						!queryResult && setShortcutId();
 					}
-				} else {
-					!queryResult && setShortcutId();
-				}
-			}}
-		>
-			<div className='relative mx-auto w-full h-16 sm:h-full overflow-hidden rounded-t-lg mb-3'>
-				{shortcut.img ? (
-					<Image
-						alt='The Icon has Failed to Load'
-						className='m-auto max-w-16 max-h-16 h-16 sm:h-auto sm:max-w-32 sm:max-h-32'
-						src={convertUrlToExternalImageUrl(shortcut.img)}
-						fill
-						sizes='100px'
-					/>
-				) : (
-					<span className='text-6xl sm:text-9xl select-none'>?</span>
-				)}
+				}}
+			>
+				<div className='relative mx-auto w-full h-16 sm:h-full overflow-hidden rounded-t-lg mb-3'>
+					{shortcut.img ? (
+						<Image
+							alt='The Icon has Failed to Load'
+							className='m-auto max-w-16 max-h-16 h-16 sm:h-auto sm:max-w-32 sm:max-h-32'
+							src={convertUrlToExternalImageUrl(shortcut.img)}
+							fill
+							sizes={((parseInt(imageQuality || '75') || 75) * 3).toString() + 'px'}
+							quality={parseInt(imageQuality || '75') || 75}
+						/>
+					) : (
+						<span className='text-6xl sm:text-9xl select-none'>?</span>
+					)}
+				</div>
+				<div className='h-12'>
+					{shortcut.name ? (
+						<b className={'line-clamp-2 ' + (shortcut.group ? 'text-yellow-400' : 'text-blue-400')}>
+							{hideIcons === 'false' && (shortcut.group ? '📁' : '🔗')} {shortcut.name}
+						</b>
+					) : (
+						<span className='text-blue-300 underline cursor-pointer'>Edit Shortcut</span>
+					)}
+				</div>
 			</div>
-			<div className='h-12'>
-				{shortcut.name ? (
-					<b className={'line-clamp-2 ' + (shortcut.group ? 'text-yellow-400' : 'text-blue-400')}>
-						{shortcut.group ? '📁' : '🔗'} {shortcut.name}
-					</b>
-				) : (
-					<span className='text-blue-300 underline cursor-pointer'>Edit Shortcut</span>
-				)}
-			</div>
-		</div>
+		)
 	);
 };
 
