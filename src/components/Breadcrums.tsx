2416880b9c6925e6ee2b16d3389a1a75c71fbd75
neinja007 <anton.siligan@gmail.com>
2024-08-20 18:21:15 +0200

    improve transition of breadcrums

--- src/components/Breadcrums.tsx
+++ src/components/Breadcrums.tsx
@@ -12,7 +12,7 @@ const Breadcrums = ({ groups, setGroups }: BreadcrumsProps) => {
 				<Fragment key={i}>
 					<button
 						onClick={() => setGroups((prev) => prev.slice(0, prev.length - (2 - i)))}
-						className='mx-2 max-w-[150px] truncate rounded-lg border border-blue-800 bg-blue-950 px-2 py-1 text-white shadow-md shadow-blue-600 transition-all hover:text-xl hover:shadow-sky-300 active:bg-blue-900'
+						className='mx-2 max-w-[150px] truncate rounded-lg border border-blue-800 bg-blue-950 px-2 py-1 text-white shadow-md shadow-blue-600 transition-all hover:-translate-y-2 hover:shadow-sky-300 active:bg-blue-900'
 					>
 						{group.replaceAll('_', ' ') || 'Home'}
 					</button>
