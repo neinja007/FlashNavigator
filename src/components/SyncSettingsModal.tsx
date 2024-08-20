d4d38b04a01ede7fb05f8edb89d7efb8ee1311e5
neinja007 <anton.siligan@gmail.com>
2024-08-20 12:56:30 +0200

    refine styles and display

--- src/components/SyncSettingsModal.tsx
+++ src/components/SyncSettingsModal.tsx
@@ -71,56 +71,61 @@ const SyncSettingsModal = ({ setSyncSettingsModal }: SyncSettingsModalProps) => 
 			</SignedOut>
 			<SignedIn>
 				<p className='text-center text-xl font-bold'>Sync Shortcuts</p>
-				<div className='mt-3 grid h-fit min-h-16 gap-3 sm:grid-cols-3'>
-					<div className='flex h-full flex-col justify-center border-b p-2 text-center sm:border-b-0 sm:border-r'>
-						<span>
-							<b>Server</b> ({downloadState === 'success' ? getShortcutCount(uploadedShortcuts) : '??'} shortcuts)
-						</span>
-						{downloadState === 'loading' && <p className='animate-pulse text-yellow-600'>Loading server data...</p>}
-						{downloadState === 'error' && <p className='text-red-400'>Failed to load server data.</p>}
-						{downloadState === 'success' && <p className='text-green-400'>Server data loaded.</p>}
-					</div>
-					<div className='flex justify-between gap-3 sm:flex-col'>
-						<button
-							className='flex w-full justify-between gap-x-2 rounded bg-blue-500 px-2 disabled:opacity-50'
-							disabled={uploadState === 'loading'}
-							onClick={upload}
-						>
-							<ChevronsLeft className='hidden sm:inline' />
-							<ChevronsUp className='sm:hidden' />
-							Upload
-							<ChevronsLeft className='hidden sm:inline' />
-							<ChevronsUp className='sm:hidden' />
-						</button>
-						<button
-							className='flex w-full justify-between gap-x-2 rounded bg-blue-500 px-2 disabled:opacity-50'
-							disabled={uploadState === 'loading' || !uploadedShortcuts || downloadState === 'loading'}
-							onClick={() => overwriteShortcuts(uploadedShortcuts)}
-						>
-							<ChevronsRight className='hidden sm:inline' />
-							<ChevronsDown className='sm:hidden' />
-							Download
-							<ChevronsRight className='hidden sm:inline' />
-							<ChevronsDown className='sm:hidden' />
-						</button>
-					</div>
-					<div className='flex h-full flex-col justify-center border-t p-2 text-center sm:border-l sm:border-t-0'>
-						<span>
-							<b>You</b> ({getShortcutCount(shortcuts)} shortcuts)
-						</span>
-						{uploadState === 'loading' && <p className='animate-pulse text-yellow-600'>Uploading...</p>}
-						{uploadState === 'error' && <p className='text-red-400'>Upload failed.</p>}
-						{uploadState === 'success' && <p className='text-green-400'>Upload successful.</p>}
+				<div className='mt-2 flex justify-center'>
+					<div className='grid h-fit min-h-16 gap-3 rounded-xl border p-3 sm:w-full sm:grid-cols-3'>
+						<div className='flex h-full flex-col justify-center border-b p-2 text-center sm:border-b-0 sm:border-r'>
+							<span>
+								<b>Server</b> ({downloadState === 'success' ? getShortcutCount(uploadedShortcuts) : '??'} shortcuts)
+							</span>
+							{downloadState === 'loading' && <p className='animate-pulse text-yellow-600'>Loading server data...</p>}
+							{downloadState === 'error' && <p className='text-red-400'>Failed to load server data.</p>}
+							{downloadState === 'success' && <p className='text-green-400'>Server data loaded.</p>}
+						</div>
+						<div className='flex justify-between gap-3 sm:flex-col'>
+							<button
+								className='flex w-full justify-between gap-x-2 rounded bg-blue-500 px-2 disabled:opacity-50'
+								disabled={uploadState === 'loading'}
+								onClick={upload}
+							>
+								<ChevronsLeft className='hidden sm:inline' />
+								<ChevronsUp className='sm:hidden' />
+								Upload
+								<ChevronsLeft className='hidden sm:inline' />
+								<ChevronsUp className='sm:hidden' />
+							</button>
+							<button
+								className='flex w-full justify-between gap-x-2 rounded bg-blue-500 px-2 disabled:opacity-50'
+								disabled={uploadState === 'loading' || !uploadedShortcuts || downloadState === 'loading'}
+								onClick={() => overwriteShortcuts(uploadedShortcuts)}
+							>
+								<ChevronsRight className='hidden sm:inline' />
+								<ChevronsDown className='sm:hidden' />
+								Download
+								<ChevronsRight className='hidden sm:inline' />
+								<ChevronsDown className='sm:hidden' />
+							</button>
+						</div>
+						<div className='flex h-full flex-col justify-center border-t p-2 text-center sm:border-l sm:border-t-0'>
+							<span>
+								<b>You</b> ({getShortcutCount(shortcuts)} shortcuts)
+							</span>
+							{uploadState === 'pending' && <p className='text-green-400'>Client data loaded.</p>}
+							{uploadState === 'loading' && <p className='animate-pulse text-yellow-600'>Uploading...</p>}
+							{uploadState === 'error' && <p className='text-red-400'>Upload failed.</p>}
+							{uploadState === 'success' && <p className='text-green-400'>Upload successful.</p>}
+						</div>
 					</div>
 				</div>
 				<div className='mt-3 text-center'>
-					{downloadState === 'success' ? (
+					{downloadState === 'success' && uploadState !== 'loading' ? (
 						inSync ? (
 							<b className='text-green-500'>Server and client (you) are in sync.</b>
 						) : (
 							<b className='text-orange-500'>Server and client (you) are not in sync.</b>
 						)
-					) : null}
+					) : (
+						<b className='animate-pulse text-gray-500'>Hold on tight...</b>
+					)}
 				</div>
 			</SignedIn>
 		</Modal>
