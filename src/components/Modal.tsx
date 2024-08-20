df19575ba4298033e398c6434be5df399c4afecc
neinja007 <anton.siligan@gmail.com>
2024-08-20 11:07:55 +0200

    add padding prop to modal component

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -4,13 +4,19 @@ import ModalBackgroundFrame from './ModalBackgroundFrame';
 type ModalProps = {
 	action?: MouseEventHandler<HTMLDivElement>;
 	children: React.ReactNode;
+	padding?: boolean;
 };
 
-const Modal = ({ children, action }: ModalProps) => {
+const Modal = ({ children, action, padding }: ModalProps) => {
 	return (
 		<>
 			<ModalBackgroundFrame action={action} />
-			<div className='fixed z-50 bg-gray-700 shadow-lg rounded-xl inset-0 mx-auto my-auto w-[700px] h-fit'>
+			<div
+				className={
+					'fixed z-50 bg-gray-700 shadow-lg rounded-xl inset-0 mx-auto my-auto w-[700px] h-fit text-left' +
+					(padding ? ' p-5' : '')
+				}
+			>
 				{children}
 			</div>
 		</>
