a00a43c08414d60def1b4d8bd1721fe34e9dc6e8
neinja007 <anton.siligan@gmail.com>
2024-08-20 11:32:48 +0200

    add catch to fetch

--- src/app/download/route.ts
+++ src/app/download/route.ts
@@ -2,18 +2,20 @@ import { currentUser } from '@clerk/nextjs/server';
 import { list } from '@vercel/blob';
 
 export async function GET() {
-  const user = await currentUser();
-  if (!user) {
-    return Response.redirect('/sign-in');
-  }
+	const user = await currentUser();
+	if (!user) {
+		return Response.redirect('/sign-in');
+	}
 
-  const blob = (await list()).blobs.find((blob) => (blob.pathname = user.id));
+	const blob = (await list()).blobs.find((blob) => (blob.pathname = user.id));
 
-  if (!blob) {
-    return Response.error();
-  }
+	if (!blob) {
+		return Response.error();
+	}
 
-  const blobData = await fetch(blob.url).then((res) => res.text());
+	const blobData = await fetch(blob.url)
+		.then((res) => res.text())
+		.catch(() => console.error("Couldn't fetch blob data"));
 
-  return Response.json({ ...blob, data: blobData });
+	return Response.json({ ...blob, data: blobData });
 }
