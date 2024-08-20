5a9b15723bd2979febc1790c4dea2c1d65eca4d5
neinja007 <anton.siligan@gmail.com>
2024-08-20 18:08:22 +0200

    add group effects for highlighting

--- src/components/Shortcut.tsx
+++ src/components/Shortcut.tsx
@@ -22,7 +22,7 @@ const Shortcut = ({ setShortcutId, setGroups, queryResult, shortcut, resetSearch
 		(!settings.hideEmptyShortcuts || shortcut.name || shortcut.href || shortcut.img) && (
 			<div
 				className={clsx(
-					'h-fit w-[calc(33.33%-12px)] cursor-pointer rounded-lg border pb-2 shadow-md transition-colors hover:border-blue-400 hover:shadow-blue-400 sm:w-[calc(25%-12px)] md:w-[calc(16.66%-12px)] xl:w-[calc(12.5%-12px)]',
+					'group h-fit w-[calc(33.33%-12px)] cursor-pointer rounded-lg border pb-2 shadow-md transition-colors hover:border-blue-400 hover:shadow-blue-400 sm:w-[calc(25%-12px)] md:w-[calc(16.66%-12px)] xl:w-[calc(12.5%-12px)]',
 					active ? 'border-blue-300 shadow-blue-400' : 'border-gray-700 shadow-gray-700',
 					settings.shortcutTypeColor && shortcut.name
 						? shortcut.group
@@ -57,7 +57,7 @@ const Shortcut = ({ setShortcutId, setGroups, queryResult, shortcut, resetSearch
 					{shortcut.img ? (
 						<Image
 							alt='The Icon has Failed to Load'
-							className='m-auto h-full max-h-20 w-full max-w-20 sm:max-h-24 sm:max-w-24 lg:max-h-28 lg:max-w-28'
+							className='m-auto h-full max-h-20 w-full max-w-20 transition-all group-hover:saturate-200 sm:max-h-24 sm:max-w-24 lg:max-h-28 lg:max-w-28'
 							src={convertUrlToExternalImageUrl(shortcut.img)}
 							fill
 							sizes={(settings.imageQuality * 3).toString() + 'px'}
@@ -67,7 +67,7 @@ const Shortcut = ({ setShortcutId, setGroups, queryResult, shortcut, resetSearch
 						<span className='select-none text-6xl sm:text-9xl'>?</span>
 					)}
 				</div>
-				<div className='flex h-12 flex-col justify-center'>
+				<div className={'flex h-12 flex-col justify-center overflow-visible transition-all group-hover:-translate-y-2'}>
 					{shortcut.name ? (
 						<b className={'line-clamp-2'}>
 							{!settings.hideShortcutIcons && (shortcut.group ? '📁' : '🔗')} {shortcut.name}
