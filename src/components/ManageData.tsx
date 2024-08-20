8271e4a468ce598b914b485a9aa51d4bbf73a5b8
neinja007 <anton.siligan@gmail.com>
2024-08-20 11:04:57 +0200

    add settings sync and collapsable controls

--- src/components/ManageData.tsx
+++ src/components/ManageData.tsx
@@ -13,8 +13,53 @@ const ManageData = () => {
   const [importDataModal, setImportDataModal] = useState(false);
   const [settings, setSettings] = useState(false);
   const [showDataControls, setShowDataControls] = useStorageState('manage-data', 'false');
+  const [syncSettingsModal, setSyncSettingsModal] = useState(false);
 
+  return (
+    <>
+      <div className='block space-x-3 text-right'>
+        {showDataControls === 'true' && (
+          <>
+            <button
+              className='text-yellow-400 hover:underline'
+              onClick={() => setDataToExport(JSON.stringify(getShortcutsObject(true)))}
+            >
+              Export Shortcuts
+            </button>
+            <button className='text-white hover:underline' onClick={() => setImportDataModal(true)}>
+              Import Shortcuts
+            </button>
+            <button className='text-blue-500 hover:underline' onClick={() => setSyncSettingsModal(true)}>
+              Sync Shortcuts
+            </button>
+            <button className='text-neutral-400 hover:underline' onClick={() => setSettings(true)}>
+              Settings
+            </button>
+          </>
+        )}
+        <button
+          className='text-purple-500 hover:underline'
+          onClick={() => setShowDataControls(showDataControls === 'false' ? 'true' : 'false')}
+        >
+          {showDataControls === 'true' ? 'Collapse' : 'Expand'} Menu
+        </button>
+        <SignedIn>
+          <div className='inline text-red-500 *:hover:underline'>
+            <SignOutButton>Sign out</SignOutButton>
+          </div>
+        </SignedIn>
+        <SignedOut>
+          <Link href={'/sign-in'} className='inline text-green-500 hover:underline'>
+            Sign in
+          </Link>
+        </SignedOut>
+      </div>
+      {syncSettingsModal && <SyncSettingsModal setSyncSettingsModal={setSyncSettingsModal} />}
+      {dataToExport && <ExportDataModal data={dataToExport} setData={setDataToExport} />}
+      {importDataModal && <ImportDataModal setImportDataModal={setImportDataModal} />}
       {settings && <SettingsModal setSettingsModal={setSettings} />}
+    </>
+  );
 };
 
 export default ManageData;
