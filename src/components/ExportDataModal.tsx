943e04dbad69e694ed1e6881005a0bff0c3ab080
neinja007 <anton.siligan@gmail.com>
2024-08-20 08:28:40 +0200

    use Modal component for existing modals

--- src/components/ExportDataModal.tsx
+++ src/components/ExportDataModal.tsx
@@ -1,5 +1,5 @@
 import { getNestedShortcuts } from '@/utils/getNestedShortcuts';
-import ModalBackgroundFrame from './ModalBackgroundFrame';
+import Modal from './Modal';
 
 type ExportDataModalProps = {
 	data: string;
@@ -10,28 +10,25 @@ const ExportDataModal = ({ data, setData }: ExportDataModalProps) => {
 	const shortcutCount = Object.keys(getNestedShortcuts()).length;
 
 	return (
-		<>
-			<ModalBackgroundFrame action={() => setData('')} />
-			<div className='fixed z-50 bg-gray-700 shadow-lg rounded-xl inset-0 mx-auto my-auto w-[700px] h-fit'>
-				<textarea
-					readOnly
-					value={data}
-					className='rounded-t-lg w-full h-96 bg-gray-800 p-2'
-					style={{ wordBreak: 'break-all' }}
-				/>
-				<div className='flex -mt-1'>
-					<button
-						className='px-2 bg-blue-500 active:bg-blue-600 rounded-bl-lg w-full py-1'
-						onClick={() => navigator.clipboard.writeText(data)}
-					>
-						Copy {shortcutCount} shortcuts
-					</button>
-					<button className='px-2 bg-red-500 active:bg-red-600 rounded-br-lg w-full py-1' onClick={() => setData('')}>
-						Close
-					</button>
-				</div>
+		<Modal action={() => setData('')}>
+			<textarea
+				readOnly
+				value={data}
+				className='rounded-t-lg w-full h-96 bg-gray-800 p-2'
+				style={{ wordBreak: 'break-all' }}
+			/>
+			<div className='flex -mt-1'>
+				<button
+					className='px-2 bg-blue-500 active:bg-blue-600 rounded-bl-lg w-full py-1'
+					onClick={() => navigator.clipboard.writeText(data)}
+				>
+					Copy {shortcutCount} shortcuts
+				</button>
+				<button className='px-2 bg-red-500 active:bg-red-600 rounded-br-lg w-full py-1' onClick={() => setData('')}>
+					Close
+				</button>
 			</div>
-		</>
+		</Modal>
 	);
 };
 
--- src/components/ImportDataModal.tsx
+++ src/components/ImportDataModal.tsx
@@ -1,8 +1,8 @@
 import { refreshStorage } from '@/utils/refreshStorage';
-import ModalBackgroundFrame from './ModalBackgroundFrame';
 import { deleteShortcutStorage } from '@/utils/deleteShortcutStorage';
 import { useState } from 'react';
 import { getNestedShortcuts } from '@/utils/getNestedShortcuts';
+import Modal from './Modal';
 
 type ImportDataModalProps = {
 	setImportDataModal: (importDataModal: boolean) => void;
@@ -25,43 +25,40 @@ const ImportDataModal = ({ setImportDataModal }: ImportDataModalProps) => {
 	const currentShortcutCount = Object.keys(getNestedShortcuts()).length;
 
 	return (
-		<>
-			<ModalBackgroundFrame action={() => setImportDataModal(false)} />
-			<div className='fixed z-50 bg-gray-700 shadow-lg rounded-xl inset-0 mx-auto my-auto w-[700px] h-fit'>
-				<textarea
-					value={dataToImport}
-					onChange={(e) => setDataToImport(e.target.value)}
-					className='rounded-t-lg w-full h-96 bg-gray-800 p-2'
-					style={{ wordBreak: 'break-all' }}
-				/>
-				<div className='flex -mt-1'>
-					<button
-						className='px-2 bg-blue-500 active:bg-blue-600 rounded-bl-lg w-1/2 py-1'
-						onClick={() => {
-							try {
-								const data = JSON.parse(dataToImport);
-								deleteShortcutStorage();
-								Object.keys(data).forEach((key) => {
-									localStorage.setItem(key.startsWith('shortcut-') ? key : 'shortcut-' + key, data[key]);
-								});
-								refreshStorage();
-								window.location.reload();
-							} catch {
-								alert('Invalid JSON data');
-							}
-						}}
-					>
-						Import {shortcutCount} shortcuts (current: {currentShortcutCount})
-					</button>
-					<button
-						className='px-2 bg-red-500 active:bg-red-600 rounded-br-lg w-1/2 py-1'
-						onClick={() => setImportDataModal(false)}
-					>
-						Cancel
-					</button>
-				</div>
+		<Modal action={() => setImportDataModal(false)}>
+			<textarea
+				value={dataToImport}
+				onChange={(e) => setDataToImport(e.target.value)}
+				className='rounded-t-lg w-full h-96 bg-gray-800 p-2'
+				style={{ wordBreak: 'break-all' }}
+			/>
+			<div className='flex -mt-1'>
+				<button
+					className='px-2 bg-blue-500 active:bg-blue-600 rounded-bl-lg w-1/2 py-1'
+					onClick={() => {
+						try {
+							const data = JSON.parse(dataToImport);
+							deleteShortcutStorage();
+							Object.keys(data).forEach((key) => {
+								localStorage.setItem(key.startsWith('shortcut-') ? key : 'shortcut-' + key, data[key]);
+							});
+							refreshStorage();
+							window.location.reload();
+						} catch {
+							alert('Invalid JSON data');
+						}
+					}}
+				>
+					Import {shortcutCount} shortcuts (current: {currentShortcutCount})
+				</button>
+				<button
+					className='px-2 bg-red-500 active:bg-red-600 rounded-br-lg w-1/2 py-1'
+					onClick={() => setImportDataModal(false)}
+				>
+					Cancel
+				</button>
 			</div>
-		</>
+		</Modal>
 	);
 };
 
