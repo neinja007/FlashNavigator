3a2b3797bae021cc5b123902e5aa757a3d437089
neinja007 <anton.siligan@gmail.com>
2024-08-20 11:03:10 +0200

    add Data Context Provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,30 +3,33 @@ import { Montserrat } from 'next/font/google';
 import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
 import { ClerkProvider } from '@clerk/nextjs';
+import { DataContextProvider } from '@/context/DataContext';
 
 const montserrat = Montserrat({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-	title: 'FlashNavigator',
-	description: 'Flash your Links quicker than the speed of light allows.'
+  title: 'FlashNavigator',
+  description: 'Flash your Links quicker than the speed of light allows.'
 };
 
 export default function RootLayout({
-	children
+  children
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<ClerkProvider>
-			<html lang='en'>
-				<head>
-					<link rel='manifest' href='/manifest.json' />
-				</head>
-				<body className={montserrat.className + ' flex justify-center'}>
-					<div className='max-w-[1240px] w-full flex justify-center pt-16 min-h-screen'>{children}</div>
-					<Analytics />
-				</body>
-			</html>
-		</ClerkProvider>
-	);
+  return (
+    <ClerkProvider>
+      <DataContextProvider>
+        <html lang='en'>
+          <head>
+            <link rel='manifest' href='/manifest.json' />
+          </head>
+          <body className={montserrat.className + ' flex justify-center'}>
+            <div className='flex min-h-screen w-full max-w-[1240px] justify-center pt-16'>{children}</div>
+            <Analytics />
+          </body>
+        </html>
+      </DataContextProvider>
+    </ClerkProvider>
+  );
 }
