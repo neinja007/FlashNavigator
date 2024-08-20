de1695242c714ffc92978364b65a9c3899c420d7
neinja007 <anton.siligan@gmail.com>
2024-08-20 11:08:49 +0200

    remove unnecessary props

--- src/components/SettingsModal.tsx
+++ src/components/SettingsModal.tsx
@@ -1,85 +1,70 @@
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useContext } from 'react';
 import ModalBackgroundFrame from './ModalBackgroundFrame';
 import SubmitButton from './SubmitButton';
+import { DataContext } from '@/context/DataContext';
 
 type SettingsModalProps = {
-	setSettings: Dispatch<SetStateAction<boolean>>;
-	hideEmptyShortcuts: string;
-	hideShortcutIcons: string;
-	imageQuality: string;
-	shortcutTypeColor: string;
-	setHideEmptyShortcuts: (hideEmptyShortcuts: string) => void;
-	setHideShortcutIcons: (hideShortcutIcons: string) => void;
-	setImageQuality: (imageQuality: string) => void;
-	setShortcutTypeColor: (shortcutTypeColor: string) => void;
+  setSettingsModal: Dispatch<SetStateAction<boolean>>;
 };
 
-const SettingsModal = ({
-	setSettings,
-	hideEmptyShortcuts,
-	hideShortcutIcons,
-	imageQuality,
-	setHideEmptyShortcuts,
-	setHideShortcutIcons,
-	setImageQuality,
-	setShortcutTypeColor,
-	shortcutTypeColor
-}: SettingsModalProps) => {
-	return (
-		<>
-			<ModalBackgroundFrame action={() => setSettings(false)} />
-			<div className='fixed z-50 bg-gray-700 shadow-lg rounded-xl text-left p-5 inset-0 mx-auto my-auto w-[700px] h-fit block'>
-				<form className='space-y-5' onSubmit={() => setSettings(false)}>
-					<div className='float-end space-x-3'>
-						<SubmitButton />
-					</div>
-					<b>Settings</b>
-					<div>
-						<input
-							type='checkbox'
-							id='hide_shortcut_icons'
-							checked={hideShortcutIcons === 'true'}
-							onChange={(e) => setHideShortcutIcons(e.target.checked.toString())}
-							className='mr-2'
-						/>
-						<label htmlFor='hide_shortcut_icons'>Hide Icons next to Shortcuts (📁 and 🔗)</label>
-					</div>
-					<div>
-						<input
-							type='checkbox'
-							id='hide_empty_shortcuts'
-							checked={hideEmptyShortcuts === 'true'}
-							onChange={(e) => setHideEmptyShortcuts(e.target.checked.toString())}
-							className='mr-2'
-						/>
-						<label htmlFor='hide_empty_shortcuts'>Hide Empty Shortcuts</label>
-					</div>
-					<div>
-						<input
-							type='checkbox'
-							id='shortcut_type_color'
-							checked={shortcutTypeColor === 'true'}
-							onChange={(e) => setShortcutTypeColor(e.target.checked.toString())}
-							className='mr-2'
-						/>
-						<label htmlFor='shortcut_type_color'>Different colors for Folders and Links</label>
-					</div>
-					<div>
-						<label htmlFor='image_quality'>Image Quality (the higher, the slower the images load):</label>
-						<input
-							type='number'
-							id='image_quality'
-							value={imageQuality || 75}
-							onChange={(e) => setImageQuality(e.target.value || '75')}
-							className='ml-2 w-20 bg-transparent border px-2 rounded-md'
-							min={0}
-							max={100}
-						/>
-					</div>
-				</form>
-			</div>
-		</>
-	);
+const SettingsModal = ({ setSettingsModal }: SettingsModalProps) => {
+  const { settings, updateSettings } = useContext(DataContext);
+
+  return (
+    <>
+      <ModalBackgroundFrame action={() => setSettingsModal(false)} />
+      <div className='fixed inset-0 z-50 mx-auto my-auto block h-fit w-[700px] rounded-xl bg-gray-700 p-5 text-left shadow-lg'>
+        <form className='space-y-5' onSubmit={() => setSettingsModal(false)}>
+          <div className='float-end space-x-3'>
+            <SubmitButton />
+          </div>
+          <b>Settings</b>
+          <div>
+            <input
+              type='checkbox'
+              id='hide_shortcut_icons'
+              checked={settings.hideShortcutIcons}
+              onChange={(e) => updateSettings('hideShortcutIcons', e.target.checked)}
+              className='mr-2'
+            />
+            <label htmlFor='hide_shortcut_icons'>Hide Icons next to Shortcuts (📁 and 🔗)</label>
+          </div>
+          <div>
+            <input
+              type='checkbox'
+              id='hide_empty_shortcuts'
+              checked={settings.hideEmptyShortcuts}
+              onChange={(e) => updateSettings('hideEmptyShortcuts', e.target.checked)}
+              className='mr-2'
+            />
+            <label htmlFor='hide_empty_shortcuts'>Hide Empty Shortcuts</label>
+          </div>
+          <div>
+            <input
+              type='checkbox'
+              id='shortcut_type_color'
+              checked={settings.shortcutTypeColor}
+              onChange={(e) => updateSettings('shortcutTypeColor', e.target.checked)}
+              className='mr-2'
+            />
+            <label htmlFor='shortcut_type_color'>Different colors for Folders and Links</label>
+          </div>
+          <div>
+            <label htmlFor='image_quality'>Image Quality (the higher, the slower the images load):</label>
+            <input
+              type='number'
+              id='image_quality'
+              value={settings.imageQuality}
+              onChange={(e) => updateSettings('imageQuality', e.target.value)}
+              className='ml-2 w-20 rounded-md border bg-transparent px-2'
+              min={10}
+              max={100}
+            />
+          </div>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default SettingsModal;
