dccff23483ba3650877d00e61bb7b7a446538bde
neinja007 <anton.siligan@gmail.com>
2024-08-20 12:56:07 +0200

    format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,28 +8,28 @@ import { DataContextProvider } from '@/context/DataContext';
 const montserrat = Montserrat({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'FlashNavigator',
-  description: 'Flash your Links quicker than the speed of light allows.'
+	title: 'FlashNavigator',
+	description: 'Flash your Links quicker than the speed of light allows.'
 };
 
 export default function RootLayout({
-  children
+	children
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <ClerkProvider>
-      <DataContextProvider>
-        <html lang='en'>
-          <head>
-            <link rel='manifest' href='/manifest.json' />
-          </head>
-          <body className={montserrat.className + ' flex justify-center'}>
-            <div className='flex min-h-screen w-full max-w-[1240px] justify-center pt-16'>{children}</div>
-            <Analytics />
-          </body>
-        </html>
-      </DataContextProvider>
-    </ClerkProvider>
-  );
+	return (
+		<ClerkProvider>
+			<DataContextProvider>
+				<html lang='en'>
+					<head>
+						<link rel='manifest' href='/manifest.json' />
+					</head>
+					<body className={montserrat.className + ' flex justify-center'}>
+						<div className='flex min-h-screen w-full max-w-[1240px] justify-center pt-16'>{children}</div>
+						<Analytics />
+					</body>
+				</html>
+			</DataContextProvider>
+		</ClerkProvider>
+	);
 }
