9776b07cde4c852020128500710354b025478a91
neinja007 <anton.siligan@gmail.com>
2024-08-20 13:27:57 +0200

    pass results

--- src/components/ShortcutQueryResults.tsx
+++ src/components/ShortcutQueryResults.tsx
@@ -1,33 +1,17 @@
 import { ShortcutType } from '@/app/page';
 import Shortcut from '@/components/Shortcut';
-import { orderShortcutsByRelevance } from '@/utils/calculateShortcutRelevanceScore';
 import { Dispatch, SetStateAction } from 'react';
 
 type ShortcutQueryResultsProps = {
-  shortcuts: { [key: string]: ShortcutType };
-  searchBarQuery: string;
-  resetSearchBarQuery: () => void;
-  setGroups: Dispatch<SetStateAction<string[]>>;
-  setShortcutId: (id: number) => void;
+	searchResults: ShortcutType[];
+	setGroups: Dispatch<SetStateAction<string[]>>;
+	searchResultsIndex: number;
 };
 
-const ShortcutQueryResults = ({
-  shortcuts,
-  searchBarQuery,
-  setGroups,
-  setShortcutId,
-  resetSearchBarQuery
-}: ShortcutQueryResultsProps) => {
-  return orderShortcutsByRelevance(Object.values(shortcuts), searchBarQuery).map((shortcut, i) => (
-    <Shortcut
-      queryResult
-      resetSearchBarQuery={resetSearchBarQuery}
-      key={i}
-      shortcut={shortcut}
-      setShortcutId={() => setShortcutId(i)}
-      setGroups={setGroups}
-    />
-  ));
+const ShortcutQueryResults = ({ searchResults, setGroups, searchResultsIndex }: ShortcutQueryResultsProps) => {
+	return searchResults.map((shortcut, i) => (
+		<Shortcut queryResult key={i} shortcut={shortcut} setGroups={setGroups} active={searchResultsIndex === i} />
+	));
 };
 
 export default ShortcutQueryResults;
