7c95139a7a61b871fd3b7e458e3d0c65494ad993
neinja007 <anton.siligan@gmail.com>
2024-08-28 16:42:10 +0200

    only update children if they are really the children of that folder

--- src/utils/updateShortcutGroupChildNames.ts
+++ src/utils/updateShortcutGroupChildNames.ts
@@ -2,19 +2,23 @@ import { getShortcutsObject } from './getShortcutsObject';
 
 export const updateShortcutGroupChildNames = (oldName: string, newName: string, pathToGroup: string) => {
 	const shortcuts = getShortcutsObject(false, true);
-	const oldNameChildPath = (
-		pathToGroup.split('-').slice(0, pathToGroup.split('-').length - 1) +
-		'-' +
-		oldName
-	).replaceAll(' ', '_');
-	const newNameChildPath = (
-		pathToGroup.split('-').slice(0, pathToGroup.split('-').length - 1) +
-		'-' +
-		newName
-	).replaceAll(' ', '_');
+
+	const pathPrefix = pathToGroup
+		.split('-')
+		.slice(0, pathToGroup.split('-').length - 1)
+		.join('-');
+
+	const oldNameChildPath = (pathPrefix ? pathPrefix + '-' + oldName : oldName).replaceAll(' ', '_');
+	const newNameChildPath = (pathPrefix ? pathPrefix + '-' + newName : newName).replaceAll(' ', '_');
 
 	const newShortcuts = Object.entries(shortcuts).reduce((acc: { [key: string]: string }, [key, value]) => {
-		acc[key.replace(oldNameChildPath, newNameChildPath)] = value;
+		const shortenedKey = key.startsWith('shortcut-') ? key.replace('shortcut-', '') : key;
+
+		if (shortenedKey.startsWith(oldNameChildPath)) {
+			acc[key.replace(oldNameChildPath, newNameChildPath)] = value;
+		} else {
+			acc[key] = value;
+		}
 		return acc;
 	}, {});
 
