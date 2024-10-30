fa90411487e6bea2940dff0da0b2d89bb97127fd
neinja007 <anton.siligan@gmail.com>
2024-10-30 09:52:05 +0100

    remove collapsible and add upload warning

--- src/components/ManageData.tsx
+++ src/components/ManageData.tsx
@@ -1,61 +1,84 @@
 import { getShortcutsObject } from '@/utils/getShortcutsObject';
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import ExportDataModal from './ExportDataModal';
 import ImportDataModal from './ImportDataModal';
 import SettingsModal from './SettingsModal';
 import { SignedIn, SignedOut, SignOutButton, useUser } from '@clerk/nextjs';
 import Link from 'next/link';
-import { FileDown, FileUp, LogIn, LogOut, Menu, RefreshCcw, Settings } from 'lucide-react';
+import { FileDown, FileUp, LogIn, LogOut, RefreshCcw, Settings } from 'lucide-react';
 import SyncShortcutsModal from './SyncShortcutsModal';
+import { DataContext } from '@/context/DataContext';
 
 const ManageData = () => {
 	const [dataToExport, setDataToExport] = useState('');
 	const [importDataModal, setImportDataModal] = useState(false);
 	const [settings, setSettings] = useState(false);
-	const [showDataControls, setShowDataControls] = useState(false);
 	const [syncShortcutsModal, setSyncShortcutsModal] = useState(false);
+	const [outOfSync, setOutOfSync] = useState(false);
+	const { shortcuts } = useContext(DataContext);
 
 	const user = useUser();
+
+	useEffect(() => {
+		fetch('/download', { cache: 'no-store' })
+			.then((res) => {
+				if (res.ok) {
+					return res.json();
+				}
+			})
+			.then((data) => {
+				const localEntries = Object.entries(getShortcutsObject(true));
+				const blobData = data.data;
+				const serverEntries = JSON.parse(blobData);
+
+				const inSync =
+					localEntries.every(([key, value]) => serverEntries[key] === value) &&
+					localEntries.length === Object.keys(serverEntries).length;
+
+				console.log('here', inSync);
+
+				setOutOfSync(!inSync);
+			});
+	}, [shortcuts]);
 
 	return (
 		<>
 			<div className='flex flex-col items-baseline space-x-3 md:flex-row'>
-				{showDataControls && (
-					<>
-						<button
-							className='flex items-center gap-x-1 text-yellow-400 transition-all hover:text-xl'
-							onClick={() => setDataToExport(JSON.stringify(getShortcutsObject(true)))}
-						>
-							<FileUp /> Export
-						</button>
-						<button
-							className='flex items-center gap-x-1 text-white transition-all hover:text-xl'
-							onClick={() => setImportDataModal(true)}
-						>
-							<FileDown /> Import
-						</button>
-						<button
-							className='flex items-center gap-x-1 text-blue-500 transition-all hover:text-xl'
-							onClick={() => setSyncShortcutsModal(true)}
-						>
-							<RefreshCcw /> Sync
-						</button>
-						<button
-							className='flex items-center gap-x-1 text-neutral-400 transition-all hover:text-xl'
-							onClick={() => setSettings(true)}
-						>
-							<Settings /> Settings
-						</button>
-					</>
-				)}
 				<button
-					className='flex items-center gap-x-1 text-purple-500 transition-all hover:text-xl'
-					onClick={() => setShowDataControls(!showDataControls)}
+					className='flex items-center gap-x-1 text-yellow-400 transition-all hover:text-xl'
+					onClick={() => setDataToExport(JSON.stringify(getShortcutsObject(true)))}
 				>
-					<Menu /> {showDataControls ? 'Collapse' : 'Expand'}
+					<FileUp /> Export
 				</button>
+				<button
+					className='flex items-center gap-x-1 text-white transition-all hover:text-xl'
+					onClick={() => setImportDataModal(true)}
+				>
+					<FileDown /> Import
+				</button>
+				<button
+					className='flex items-center gap-x-1 text-neutral-400 transition-all hover:text-xl'
+					onClick={() => setSettings(true)}
+				>
+					<Settings /> Settings
+				</button>
+				{outOfSync ? (
+					<button
+						className='flex animate-pulse items-center gap-x-1 text-red-500 transition-all hover:text-xl'
+						onClick={() => setSyncShortcutsModal(true)}
+					>
+						<RefreshCcw /> out of sync!
+					</button>
+				) : (
+					<button
+						className='flex items-center gap-x-1 text-blue-500 transition-all hover:text-xl'
+						onClick={() => setSyncShortcutsModal(true)}
+					>
+						<RefreshCcw /> Sync
+					</button>
+				)}
 				<SignedIn>
-					<button className='text-red-500 transition-all hover:text-xl'>
+					<button className='text-green-500 transition-all hover:text-xl'>
 						<SignOutButton>
 							<div className='flex items-center gap-x-1'>
 								<LogOut /> {user.user?.primaryEmailAddress?.emailAddress}
